2db8c0cde828acca68f9a8bf60fd2a757973c8ab
VedantS26 <vedantshingote07@gmail.com>
2025-01-09 13:39:29 +0530

    This is the introduction to debugging tools in intelIJ idea

---
+++ src/section_05_introduction_to_java/Example2.java
@@ -0,0 +1,16 @@
+package section_05_introduction_to_java;
+
+import java.lang.*;
+public class Example2 {
+    public static void main(String[] args) {
+        System.out.println(" Name: Vedant Shingote \n ph no. 123456789 \n email.id : vedantshingote@gmail.com \n Education Btech: 8.11 ,hsc: 95.17, ssc: 77.8");
+        System.out.println(" Skills : JAVA, C Programming, SQL, Basics of quartus-2");
+        System.out.println(" Projects Minesweeper Game,Bluetooth Bot,Solenoid Door lock");
+        System.out.println(" hobbies : Cricket");
+        System.out.println(" Objective: To utilize my technical skills for achieving the target and developing the best performance in the organization");
+        System.out.println(" Branch : Electronics and Telecommunication");
+        System.out.println(" YCCE");
+        
+
+    }
+}
